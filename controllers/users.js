5ba21de78f0dea24e592379b74022a51394a733f
Venera Nechaeva <venera_nechaeva@mail.ru>
2023-10-12 18:33:19 +0300

    feat: fix secure cookie

--- controllers/users.js
+++ controllers/users.js
@@ -58,6 +58,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: 'None',
+        secure: true,
         // вернем токен
       }).send({ token });
     })
