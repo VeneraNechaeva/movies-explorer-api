70a649599120dc5f788ef53f5149a8a0820021a3
Venera Nechaeva <venera_nechaeva@mail.ru>
2023-10-12 18:31:05 +0300

    feat: sameSite fix

--- controllers/users.js
+++ controllers/users.js
@@ -57,7 +57,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: true,
+        sameSite: 'Lax',
         // вернем токен
       }).send({ token });
     })
